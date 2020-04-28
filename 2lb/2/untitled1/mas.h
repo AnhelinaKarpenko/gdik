5a5a387839010bf9790042ebea629de324692716
AnhelinaKarpenko <63188559+AnhelinaKarpenko@users.noreply.github.com>
2020-04-28 10:45:00 +0300

    работа с кодом

--- 2lb/2/untitled1/mas.h
+++ 2lb/2/untitled1/mas.h
@@ -1,14 +1,4 @@
-#ifndef MAS_H
-#define MAS_H
 
-class mas
-{
-    int *arr;
-    int size;
-public:
-    mas(int count);
-    mas(const mas &obj);
-    void output();
     void input();
     mas & association(mas&obj); //объединение
     mas & intersection(mas&obj); // пересечение
@@ -17,7 +7,10 @@ public:
     void status(); // проверка приналежности числа
     void inclusion (); // проверка включения подмножества
     ~mas();
-
+    & intersection(mas&obj); // пересечение
+       mas & difference(mas& & intersection(mas&obj); // пересечение
+       mas & difference(mas& & intersection(mas&obj); // пересечение
+       mas & difference(mas&
 };
 
 #endif // MAS_H
